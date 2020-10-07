8dc89ac1b88703a904f940c50ebc2682289df1fa
calmisential <xinyu_std@163.com>
2020-10-07 09:32:14 +0800

    优化代码

--- java_solutions/src/剑指offer/P29_顺时针打印矩阵/Main.java
+++ java_solutions/src/剑指offer/P29_顺时针打印矩阵/Main.java
@@ -1,21 +1,47 @@
 package 剑指offer.P29_顺时针打印矩阵;
 
 
+import java.util.Arrays;
+
 class Solution {
-    private int[] move(int x, int y, int ori){
-        int[] xy = new int[2];
-        if (ori%4 == 0){
-            y += 1;
-        }else if (ori%4 == 1){
-            x += 1;
-        }else if (ori%4==2){
-            y -= 1;
+    private int x;
+    private int y;
+    private boolean[][] visited;
+    private int m;
+    private int n;
+    private int moveOrientation;
+
+    private boolean checkValid(int x, int y){
+        return x >= 0 && y >= 0 && x < m && y < n && !visited[x][y];
+    }
+
+
+    private void move(){
+        if (moveOrientation%4 == 0){
+            if (checkValid(x, y+1)){
+                y += 1;
+            }else{
+                moveOrientation++;
+            }
+        }else if (moveOrientation%4 == 1){
+            if (checkValid(x+1, y)){
+                x += 1;
+            }else{
+                moveOrientation++;
+            }
+        }else if (moveOrientation%4==2){
+            if (checkValid(x, y-1)){
+                y -= 1;
+            }else{
+                moveOrientation++;
+            }
         }else{
-            x -= 1;
+            if (checkValid(x-1, y)){
+                x -= 1;
+            }else{
+                moveOrientation++;
+            }
         }
-        xy[0] = x;
-        xy[1] = y;
-        return xy;
     }
 
 
@@ -24,37 +50,28 @@ class Solution {
             return new int[0];
         }
         // m行n列
-        int m = matrix.length;
-        int n = matrix[0].length;
+        m = matrix.length;
+        n = matrix[0].length;
         int[] ans = new int[m*n];
-        boolean[][] visited = new boolean[m][n];
+        visited = new boolean[m][n];
 
         // 4个移动方向，0, 1, 2, 3
-        int moveOrientation = 0;
+        moveOrientation = 0;
         int idx = 1;
-        int x = 0;
-        int y = 0;
+        x = 0;
+        y = 0;
 
         ans[0] = matrix[0][0];
         visited[0][0] = true;
 
         while (idx < m*n){
-
-            int[] newXY = move(x, y, moveOrientation);
-            int newX = newXY[0];
-            int newY = newXY[1];
-            if ((newX < 0 || newY < 0 || newX >= m || newY >= n) || (visited[newX][newY])){
-                moveOrientation++;
-                newX = x;
-                newY = y;
-            }else{
-                ans[idx] = matrix[newX][newY];
-                visited[newX][newY] = true;
+            int temp = moveOrientation;
+            move();
+            if (temp == moveOrientation){
+                ans[idx] = matrix[x][y];
+                visited[x][y] = true;
                 idx++;
             }
-
-            x = newX;
-            y = newY;
         }
 
         return ans;
@@ -65,6 +82,12 @@ class Solution {
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
+        Solution s = new Solution();
+        int[][] mat = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        System.out.println(Arrays.toString(s.spiralOrder(mat)));
     }
 }
---
+++ python题解/1~100/0075_颜色分类.py
@@ -0,0 +1,15 @@
+
+from typing import List
+
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        pass
+
+
+
+if __name__ == '__main__':
+    print(Solution().sortColors([2,0,2,1,1,0]))
