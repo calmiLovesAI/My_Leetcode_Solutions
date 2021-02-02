41756cc2ef6bf74da3e122ca9093798c61d08fc6
calmisential <xinyu_std@163.com>
2021-02-02 21:30:09 +0800

    分隔链表

---
+++ java_solutions/src/from_1_to_200/P86_分隔链表/Main.java
@@ -0,0 +1,62 @@
+package from_1_to_200.P86_分隔链表;
+
+import java.util.List;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode small = new ListNode();
+        ListNode smallHead = small;
+        ListNode large = new ListNode();
+        ListNode largeHead = large;
+        while (head != null){
+            if (head.val < x){
+                small.next = head;
+                small = small.next;
+            }else{
+                large.next = head;
+                large = large.next;
+            }
+            head = head.next;
+        }
+
+        small.next = largeHead.next;
+        large.next = null;
+        return smallHead.next;
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
