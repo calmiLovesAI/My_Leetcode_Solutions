f91e88ed642113125b10296257a5a0d7d3cb8eb1
calmisential <xinyu_std@163.com>
2021-03-03 16:48:55 +0800

    3.4

---
+++ java_solutions/src/from_601_to_800/P746_使用最小花费爬楼梯/Main.java
@@ -0,0 +1,22 @@
+package from_601_to_800.P746_使用最小花费爬楼梯;
+
+
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n+1];
+        dp[0] = dp[1] = 0;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]);
+        }
+        return dp[n];
+    }
+}
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
