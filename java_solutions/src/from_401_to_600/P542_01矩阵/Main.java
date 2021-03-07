fb0fad0dae02ed1e4cf03fbffd20acece84c2d59
calmisential <xinyu_std@163.com>
2021-03-07 20:48:48 +0800

    动态规划

---
+++ java_solutions/src/from_401_to_600/P542_01矩阵/Main.java
@@ -0,0 +1,83 @@
+package from_401_to_600.P542_01矩阵;
+
+import java.util.Arrays;
+
+
+// 动态规划
+class Solution {
+    public int[][] updateMatrix(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+
+        // dp[i][j] 表示位置(i, j) 到最近的0的距离
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(dp[i], Integer.MAX_VALUE / 2);
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0){
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        // 只有 水平向左移动 和 竖直向上移动，注意动态规划的计算顺序
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i - 1 >= 0) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + 1);
+                }
+                if (j - 1 >= 0) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][j - 1] + 1);
+                }
+            }
+        }
+        // 只有 水平向左移动 和 竖直向下移动，注意动态规划的计算顺序
+        for (int i = m - 1; i >= 0; --i) {
+            for (int j = 0; j < n; ++j) {
+                if (i + 1 < m) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i + 1][j] + 1);
+                }
+                if (j - 1 >= 0) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][j - 1] + 1);
+                }
+            }
+        }
+        // 只有 水平向右移动 和 竖直向上移动，注意动态规划的计算顺序
+        for (int i = 0; i < m; ++i) {
+            for (int j = n - 1; j >= 0; --j) {
+                if (i - 1 >= 0) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + 1);
+                }
+                if (j + 1 < n) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][j + 1] + 1);
+                }
+            }
+        }
+        // 只有 水平向右移动 和 竖直向下移动，注意动态规划的计算顺序
+        for (int i = m - 1; i >= 0; --i) {
+            for (int j = n - 1; j >= 0; --j) {
+                if (i + 1 < m) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i + 1][j] + 1);
+                }
+                if (j + 1 < n) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][j + 1] + 1);
+                }
+            }
+        }
+
+        return dp;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(new Solution().updateMatrix(
+                new int[][]{
+                        {0, 0, 0},
+                        {0, 1, 0},
+                        {0, 0, 0}
+                }
+        )));
+    }
+}
