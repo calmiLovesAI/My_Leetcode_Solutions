a892f90df7e5ded9c3cd1da5c963836b6df1f35a
calmisential <xinyu_std@163.com>
2021-04-03 23:02:05 +0800

    最长公共子序列

---
+++ java_solutions/src/more_than_1001/P1143_最长公共子序列/Main.java
@@ -0,0 +1,30 @@
+package more_than_1001.P1143_最长公共子序列;
+
+//  二维动态规划
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[][] dp = new int[m+1][n+1];
+        for (int i = 1; i <= m; i++) {
+            char c1 = text1.charAt(i-1);
+            for (int j = 1; j <= n; j++) {
+                char c2 = text2.charAt(j-1);
+                if (c1 == c2){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().longestCommonSubsequence("abcde", "ace"));
+    }
+}
