bf74ae8077836303028112318f7c67a559f379ee
calmisential <xinyu_std@163.com>
2021-06-19 13:07:02 +0800

    更新

---
+++ java_solutions/src/more_than_1001/P1239_串联字符串的最大长度/Main.java
@@ -0,0 +1,51 @@
+package more_than_1001.P1239_串联字符串的最大长度;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+//  dfs + 位运算
+class Solution {
+    int ans = 0;
+
+    public int maxLength(List<String> arr) {
+        ArrayList<Integer> masks = new ArrayList<>();
+        for (String s : arr) {
+            int mask = 0;
+            for (int i = 0; i < s.length(); ++i) {
+                int ch = s.charAt(i) - 'a';
+                if (((mask >> ch) & 1) != 0) { // 若 mask 已有 ch，则说明 s 含有重复字母，无法构成可行解
+                    mask = 0;
+                    break;
+                }
+                mask |= 1 << ch; // 将 ch 加入 mask 中
+            }
+            if (mask > 0) {
+                masks.add(mask);
+            }
+        }
+
+        backtrack(masks, 0, 0);
+
+        return ans;
+    }
+
+    public void backtrack(List<Integer> masks, int pos, int mask) {
+        if (pos == masks.size()) {
+            ans = Math.max(ans, Integer.bitCount(mask));
+            return;
+        }
+        if ((mask & masks.get(pos)) == 0) { // mask 和 masks[pos] 无公共元素
+            backtrack(masks, pos + 1, mask | masks.get(pos));
+        }
+        backtrack(masks, pos + 1, mask);
+    }
+
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
