3d550592a85a52c42d883c06fb727a917b4f93a4
calmisential <xinyu_std@163.com>
2021-06-22 16:42:16 +0800

    全排列

---
+++ java_solutions/src/剑指offer/P38_字符串的排列/Main.java
@@ -0,0 +1,61 @@
+package 剑指offer.P38_字符串的排列;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution {
+    public String[] permutation(String s) {
+        ArrayList<String> ret = new ArrayList<>();
+        char[] arr = s.toCharArray();
+        Arrays.sort(arr);
+        do {
+            ret.add(new String(arr));
+        }while (nextPermutation(arr));
+        int size = ret.size();
+        String[] retArr = new String[size];
+        for (int i = 0; i < size; i++) {
+            retArr[i] = ret.get(i);
+        }
+        return retArr;
+    }
+
+
+    public boolean nextPermutation(char[] arr) {
+        int i = arr.length - 2;
+        while (i >= 0 && arr[i] >= arr[i + 1]) {
+            i--;
+        }
+        if (i < 0) {
+            return false;
+        }
+        int j = arr.length - 1;
+        while (j >= 0 && arr[i] >= arr[j]) {
+            j--;
+        }
+        swap(arr, i, j);
+        reverse(arr, i + 1);
+        return true;
+    }
+
+    public void swap(char[] arr, int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public void reverse(char[] arr, int start) {
+        int left = start, right = arr.length - 1;
+        while (left < right) {
+            swap(arr, left, right);
+            left++;
+            right--;
+        }
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
